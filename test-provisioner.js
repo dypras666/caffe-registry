d154eb0060c69c499f13a4b0155622d549d36a59
Kurniawan <dypras@icloud.com>
2026-09-22 21:18:21 +0700

    chore: remove test files and fix imports

--- test-provisioner.js
+++
@@ -1,6 +0,0 @@
-const p = require('./services/provisioner');
-console.log('Testing provisioner module...');
-
-p.checkAvailability('test123')
-  .then(r => console.log('checkAvailability result:', r))
-  .catch(e => console.error('Error:', e.message));
